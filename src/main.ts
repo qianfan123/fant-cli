4f17f7cd405ac36b33af8f2bf8dc62ced706616f
songjian <songjian@hd123.com>
2019-03-19 10:08:54 +0800

    import fant3

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,19 @@
-import Vue from 'vue';
-import App from './App.vue';
-import router from './router';
-import './assets/iconfont/iconfont.css';
-import './registerServiceWorker';
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import './assets/iconfont/iconfont.css'
+import './registerServiceWorker'
+import fant3 from 'fant3'
 
-import Filter from './cmd/Filter.js';
+import Filter from './cmd/Filter.js'
 
-import i18n from './locale/Locale.js';
-Vue.config.productionTip = false;
-Filter.init();
+import i18n from './locale/Locale.js'
+
+Vue.use(fant3)
+Vue.config.productionTip = false
+Filter.init()
 new Vue({
   i18n,
   render: (h) => h(App),
-  router,
-}).$mount('#app');
+  router
+}).$mount('#app')
