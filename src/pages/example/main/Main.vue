de94745c64cf0ea01b873275df014ea4b05a8156
qianfan123 <1553861774@qq.com>
2019-03-20 17:29:51 +0800

    导航

--- src/pages/example/main/Main.vue
+++ src/pages/example/main/Main.vue
@@ -4,19 +4,11 @@
             <div class="nav-title">测试管理系统</div>
             <el-menu :default-openeds="['1', '3']" class="menu-width">
                 <el-submenu index="1">
-                    <template slot="title"><i class="el-icon-message"></i>导航一</template>
+                    <template slot="title"><i class="el-icon-message"></i>测试</template>
                     <el-menu-item-group class="nav-li-group">
                         <template slot="title">分组一</template>
-                        <el-menu-item index="1-1">选项1</el-menu-item>
-                        <el-menu-item index="1-2">选项2</el-menu-item>
+                        <el-menu-item index="1-1">示例管理</el-menu-item>
                     </el-menu-item-group>
-                    <el-menu-item-group title="分组2" class="nav-li-group">
-                        <el-menu-item index="1-3">选项3</el-menu-item>
-                    </el-menu-item-group>
-                    <el-submenu index="1-4">
-                        <template slot="title">选项4</template>
-                        <el-menu-item index="1-4-1">选项4-1</el-menu-item>
-                    </el-submenu>
                 </el-submenu>
                 <el-submenu index="2">
                     <template slot="title"><i class="el-icon-menu"></i>导航二</template>
