20f48fd38649cfac75a79c80a29aef9368ab5515
songjian <songjian@hd123.com>
2019-03-26 13:06:41 +0800

    FANT-107

--- src/pages/main/Main.ts
+++ src/pages/main/Main.ts
@@ -1,23 +1,25 @@
-import { Component, Vue, Watch } from 'vue-property-decorator'
+import { Component, Vue } from 'vue-property-decorator'
 
 @Component({
-    components: {}
+  components: {}
 })
 export default class Main extends Vue {
-    isCollapse: boolean = false
-    onMenuClick() {
-        this.$confirm('是否确认退出当前账户?', '退出登录', {
-            confirmButtonText: '确定',
-            cancelButtonText: '取消'
-        }).then(() => {
-            this.$router.push({ name: 'login' })
-        })
-    }
-    /**
-     * 侧边栏折叠
-     */
-    onCollapse() {
-        this.isCollapse = !this.isCollapse
-    }
+  isCollapse: boolean = false
+
+  onMenuClick() {
+    this.$confirm('是否确认退出当前账户?', '退出登录', {
+      confirmButtonText: '确定',
+      cancelButtonText: '取消'
+    }).then(() => {
+      this.$router.push({ name: 'login' })
+    })
+  }
+
+  /**
+   * 侧边栏折叠
+   */
+  onCollapse() {
+    this.isCollapse = !this.isCollapse
+  }
 }
 
