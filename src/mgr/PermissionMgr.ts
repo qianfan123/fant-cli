4ffd735fcb71afae663224b56c71d914b2963a7c
qianfan123 <1553861774@qq.com>
2019-04-09 17:18:48 +0800

    FANT-124

--- src/mgr/PermissionMgr.ts
+++ src/mgr/PermissionMgr.ts
@@ -5,10 +5,49 @@ export default class PermissionMgr {
   /**
    * 界面权限
    * 方法的原理：
-   * 1、用户登录成功后，返回该用户的一个数组，数组中存放的是该用户拥有的权限
+   * 1、用户登录成功后，返回该用户的一个数组，数组中存放的是该用户拥有的权限,存在vuex中的permission字段下
    * 2、后端返回一个数组，该数组存放着菜单的数组，数组中每一条数据中都存在着这个菜单所需要的权限
    * 3、通过对比权限，获取用户能够显示的菜单
    * @param menus
+   * 示例代码：
+   * 用户登录成功后返回用户的权限数组如：
+   *[{
+   *     code: "purchase.purchase.view",
+   *     name: "采购.进货.查询",
+   *     caption: null,
+   *     children: null
+   *},...]
+   * 在main.vue加载的时候，获取菜单数组，这里假设叫做arr：
+   [{
+     id: "menu2",
+     version: 0,
+     created: "2018-10-16 19:29:44",…}，
+     children:[{id: "menu2-1-3", version: 0, created: "2019-01-30 12:42:04",…},…]，
+     code:null，
+     created:"2018-10-16 19:29:44"，
+     creator:{id: "init", code: "init", name: "init"}，
+     firName:"采购"，
+     home:false，
+     icon:"ic-ic_stock"，
+     id:"menu2"，
+     lastModified:"2019-03-04 13:34:07"，
+     lastModifier:{id: "init", code: "init", name: "init"}，
+     name:"采购"，
+     permissions:["purchase.purchase.view", "purchase.purchaseReturn.view", "purchase.purchaseOrder.view",purchase.purchaseAdjustment.view]，
+     secChildren:null，
+     secName:null，
+     secUrl:null，
+     sequence:1，
+     thirdChildren:null，
+     thirdName:null，
+     upper:null，
+     url:null，
+     version:0
+   },...]
+   * 使用方法：
+   * PermissionMgr.filterMenus(arr).then((menu: any) => {
+          this.backendMenus = menu
+        })
    */
   static filterMenus(menus: any[]): any {
     return new Promise((resolve) => {
@@ -63,6 +102,10 @@ export default class PermissionMgr {
   /**
    * 操作权限
    * @param permission
+   * 使用举例：
+   * hasPermissions: Function = PermissionMgr.hasPermission(当然也可以放在Vue的prototype上来使用，
+   * 如Vue.prototype.hasPermissions = PermissionMgr.hasPermissions)
+   * v-if="hasPermissions('finance.receivable.create')"
    */
   static hasPermissions(permission: string) {
     let storePermission = store.state.permission
