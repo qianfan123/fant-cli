9b19cd137ae2888a2d30ef6d2a9033aa4d962acd
qianfan123 <1553861774@qq.com>
2019-04-09 16:05:03 +0800

    FANT-124

--- src/mgr/PermissionMgr.ts
+++ src/mgr/PermissionMgr.ts
@@ -1,3 +1,84 @@
+import ObjectUtil from 'util/ObjectUtil.js'
+import store from '../store'
+
 export default class PermissionMgr {
-  // todo
+  /**
+   * 界面权限
+   * 方法的原理：
+   * 1、后端返回一个数组，该数组存放着系统的所有权限
+   * 2、用户登录成功后，返回该用户的一个数组，数组中存放的是该用户拥有的权限
+   * 3、通过对比权限，获取用户能够显示的菜单
+   * @param menus
+   */
+  static filterMenus(menus: any[]): any {
+    return new Promise((resolve) => {
+      let result: any[] = []
+      menus = ObjectUtil.copy(menus)
+      menus.forEach(group => {
+        if (PermissionMgr.hasMenuPermissions(group.permissions)) {
+          if (group.children && group.children.length >= 0) {
+            let children: any[] = []
+            group.children.forEach((item: any) => {
+              if (PermissionMgr.hasMenuPermissions(item.permissions)) {
+                children.push(item)
+              }
+            })
+            group.children = children
+            result.push(group)
+          } else {
+            result.push(group)
+          }
+        }
+      })
+      resolve(result)
+    })
+  }
+
+  static hasMenuPermissions(permissions: string[] | string) {
+    if (!permissions) {
+      return true
+    }
+    if (permissions.length === 0) {
+      return true
+    }
+    if (!store.state.permission || store.state.permission.length === 0) {
+      return false
+    }
+    let result: any = []
+    console.log(store.state.permission)
+    for (let i = 0; i < store.state.permission.length; i++) {
+      for (let j = 0; j < permissions.length; j++) {
+        if (store.state.permission[i].code === permissions[j]) {
+          result.push(store.state.permission[i])
+        }
+      }
+    }
+    if (result.length > 0) {
+      return true
+    } else {
+      return false
+    }
+  }
+
+  /**
+   * 操作权限
+   * @param permission
+   */
+  static hasPermissions(permission: string) {
+    let storePermission = store.state.permission
+    if (!permission) {
+      return true
+    }
+    if (!storePermission || storePermission.length === 0) {
+      return false
+    }
+    let filterItem = storePermission.filter((item: any) => {
+      return item.code === permission
+    })
+    if (filterItem.length > 0) {
+      return true
+    } else {
+      return false
+    }
+  }
 }
